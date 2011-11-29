b561a3d4f8b9a10a9aab68d15729266978b27b0b
happy <happy@doc5.com>
2011-11-29 22:04:18 +0800

    add sites of 114la

---
+++ publics/tools/chmod.sh
@@ -0,0 +1,19 @@
+chmod 777 ../../sites/install 
+chmod 777 ../../sites/static/js 
+chmod 777 ../../sites/admin/data 
+chmod 777 ../../sites/admin/data/backup 
+chmod 777 ../../sites/admin/data/cache 
+chmod 777 ../../sites/admin/data/conf 
+chmod 777 ../../sites/admin/data/db 
+chmod 777 ../../sites/admin/data/session 
+chmod 777 ../../sites/admin/data/lang 
+chmod 777 ../../sites/admin/data/log 
+chmod 777 ../../sites/admin/data/plan 
+chmod 777 ../../sites/admin/data/update 
+chmod 777 ../../sites/admin/tpls/cache 
+chmod 777 ../../sites/admin/tpls/compile 
+chmod 777 ../../sites/admin/tpls/tpls/admin 
+chmod 777 ../../sites/admin/tpls/tpls/main 
+chmod 777 ../../sites/admin/config/cfg_database.php
+
+
