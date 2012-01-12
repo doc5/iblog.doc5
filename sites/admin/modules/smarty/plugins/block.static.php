7ed319abd2184f6799ea8e36bcad979db75135b7
happy <happy@doc5.com>
2012-01-12 05:27:26 +0300

    delete sites

--- sites/admin/modules/smarty/plugins/block.static.php
+++
@@ -1,8 +0,0 @@
-<?php
-!defined('PATH_ADMIN') && exit('Forbidden');
-function smarty_block_static ($params, $content, &$smarty, &$repeat)
-{
-    cls_template::assign('STATIC', $content);
-    return '';
-}
-?>
--- sites/admin/tpls/tpls/admin/ip_deny_list.tpl
+++
@@ -1,4 +0,0 @@
-<form method="post" action="?c=config&a=ip_deny">
-以下为被禁止的IP:<textarea name='ip_deny_list'><{$ip_deny_list}></textarea><br />
-<input type='submit' />
-</form>
